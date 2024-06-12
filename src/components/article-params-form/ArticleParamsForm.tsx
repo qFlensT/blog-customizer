421d22b9b6d8869529a1def7ad50a60aa257b568
Evgeniy Bondarenko <zhekos1123@ya.ru>
2024-06-12 14:46:42 +0500

    refactoring(components/article-params-form): add form elements and state handle

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,88 @@
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
-import { FormEvent, ReactNode } from 'react';
+import { FormEvent, ReactNode, useState } from 'react';
+import { Text } from '../text';
+import { Select } from '../select';
+import {
+	OptionType,
+	backgroundColors,
+	contentWidthArr,
+	defaultArticleState,
+	fontColors,
+	fontFamilyOptions,
+	fontSizeOptions,
+} from 'src/constants/articleProps';
+import { RadioGroup } from '../radio-group';
+import { Separator } from '../separator';
 
 export type ArticleParamsFormProps = {
 	children?: ReactNode;
-	onSubmit?: (e: FormEvent) => void;
+	onSubmit?: (value: typeof defaultArticleState) => void;
+	onReset?: (value: typeof defaultArticleState) => void;
 };
 
 export const ArticleParamsForm = ({
-	children,
 	onSubmit,
+	onReset,
 }: ArticleParamsFormProps) => {
+	const [articleParams, setArticleParams] =
+		useState<typeof defaultArticleState>(defaultArticleState);
+
+	const handleSetParams = (
+		keyName: keyof typeof articleParams,
+		value: OptionType
+	) => {
+		setArticleParams((prev) => ({ ...prev, [keyName]: value }));
+	};
+
+	const handleReset = () => {
+		setArticleParams(defaultArticleState);
+		onReset?.(defaultArticleState);
+	};
+
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault();
+		onSubmit?.(articleParams);
+	};
+
 	return (
-		<form className={styles.form} onSubmit={onSubmit}>
-			{children}
+		<form className={styles.form} onSubmit={handleSubmit} onReset={handleReset}>
+			<Text as='h2' size={31} weight={800} uppercase>
+				Задайте параметры
+			</Text>
+			<Select
+				title='Шрифт'
+				options={fontFamilyOptions}
+				selected={articleParams.fontFamilyOption}
+				onChange={(v) => handleSetParams('fontFamilyOption', v)}
+			/>
+			<RadioGroup
+				title='Размер шрифта'
+				name='font-size'
+				options={fontSizeOptions}
+				selected={articleParams.fontSizeOption}
+				onChange={(v) => handleSetParams('fontSizeOption', v)}
+			/>
+			<Select
+				title='Цвет шрифта'
+				options={fontColors}
+				selected={articleParams.fontColor}
+				onChange={(v) => handleSetParams('fontColor', v)}
+			/>
+			<Separator />
+			<Select
+				title='Цвет фона'
+				options={backgroundColors}
+				selected={articleParams.backgroundColor}
+				onChange={(v) => handleSetParams('backgroundColor', v)}
+			/>
+			<Select
+				title='Ширина контента'
+				options={contentWidthArr}
+				selected={articleParams.contentWidth}
+				onChange={(v) => handleSetParams('contentWidth', v)}
+			/>
 			<div className={styles.bottomContainer}>
 				<Button title='Сбросить' type='reset' />
 				<Button title='Применить' type='submit' />
--- src/index.tsx
+++ src/index.tsx
@@ -9,10 +9,6 @@ import { defaultArticleState } from './constants/articleProps';
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
 import { Aside } from './components/aside';
-import { Text } from './components/text';
-import { Select } from './components/select';
-import { RadioGroup } from './components/radio-group';
-import { Separator } from './components/separator';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
@@ -31,42 +27,7 @@ const App = () => {
 				} as CSSProperties
 			}>
 			<Aside>
-				<ArticleParamsForm>
-					<Text as='h2' size={31} weight={800} uppercase>
-						Задайте параметры
-					</Text>
-					<Select
-						options={[{ title: 'Open Sans', value: 'opensans', className: '' }]}
-						selected={null}
-						title='Шрифт'
-					/>
-					<RadioGroup
-						title='Размер шрифта'
-						name='font-size'
-						options={[{ title: 'Open Sans', value: 'opensans', className: '' }]}
-						selected={{
-							title: 'Open Sans',
-							value: 'opensans',
-							className: '',
-						}}
-					/>
-					<Select
-						options={[{ title: 'Open Sans', value: 'opensans', className: '' }]}
-						selected={null}
-						title='Цвет шрифта'
-					/>
-					<Separator />
-					<Select
-						options={[{ title: 'Open Sans', value: 'opensans', className: '' }]}
-						selected={null}
-						title='Цвет фона'
-					/>
-					<Select
-						options={[{ title: 'Open Sans', value: 'opensans', className: '' }]}
-						selected={null}
-						title='Ширина контента'
-					/>
-				</ArticleParamsForm>
+				<ArticleParamsForm></ArticleParamsForm>
 			</Aside>
 			<Article />
 		</div>
